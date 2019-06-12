783d0e77081b59cb04bdd1ee8f5d647686b88bcb
nick <nick@bigbox-store.com>
2019-06-12 10:39:26 -0400

    Added lsb-core lines for most Linux systems

--- install_duo_unix.sh
+++ install_duo_unix.sh
@@ -4,9 +4,14 @@
 # Capture Operating System
 if lsb_release > /dev/null 2>&1 ; then
     read OS <<< $(lsb_release -d | awk '/Description/')
-    echo "{$OS}"
+    echo "$OS"
 else
-    echo "lsb_release package required to proceed"
-    # Centos/Redhat - redhat-lsb-core
-    # Ubuntu - sb-core
+    echo "'lsb_release' package required to proceed"
+    # Centos/Redhat: sudo yum install redhat-lsb-core
+    # Ubuntu/Debian: sudo apt-get install lsb-core
+    # Fedora: sudo dnf install redhat-lsb-core
+    # OpenSUSE: sudo zypper install lsb-core
+    # Arch: pacman -Syu lsb-release
 fi
+
+echo "$OS"
