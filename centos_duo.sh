0fa59b35ff98e8adc7774e0e7b358a2821aef11d
nick <nick@bigbox-store.com>
2019-06-12 09:12:15 -0400

    breaking out files

---
+++ centos_duo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Purpose: Configure CentOS based on operating system results 
+
+# Install pam_duo prerequisites
+# Download and extract the latest version of duo_unix
+# Build and install duo_unix with PAM support
+# Update /etc/duo/pam_duo.conf with IKEY, SKEY, and HOST
+# PubKey + Duo or Password + Duo?
+# Configure /etc/pam.d/
+
---
+++ ubuntu_duo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Purpose: Configure Ubuntu based on operating system results 
+
+# Install pam_duo prerequisites
+# Download and extract the latest version of duo_unix
+# Build and install duo_unix with PAM support
+# Update /etc/duo/pam_duo.conf with IKEY, SKEY, and HOST
+# PubKey + Duo or Password + Duo?
+# Configure /etc/pam.d/
+
