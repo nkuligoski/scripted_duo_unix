014abbb0f62e0a44713e4aafe7d368c0ebe6e76d
nick <nick@bigbox-store.com>
2019-06-12 10:59:51 -0400

    starting logic

--- install_duo_unix.sh
+++ install_duo_unix.sh
@@ -2,7 +2,7 @@
 # Purpose: Assist administrators install Duo on CentOS and Ubuntu
 
 # Capture Operating System
-if lsb_release > /dev/null 2>&1 ; then
+if [ lsb_release > /dev/null 2>&1 ]; then
     read OS <<< $(lsb_release -d | awk '/Description/')
     echo "$OS"
 else
@@ -12,6 +12,13 @@ else
     # Fedora: sudo dnf install redhat-lsb-core
     # OpenSUSE: sudo zypper install lsb-core
     # Arch: pacman -Syu lsb-release
+    exit(0)
 fi
 
-echo "$OS"
+# Will need to either shorten the OS variable to major system versions
+# or use regex in the if statement so that it doesnt care about the minor version
+if [ "$OS" =~ "Ubuntu 16.04.6 LTS" ]; then
+	echo "Call 'ubuntu_duo' script"
+else
+	echo "Couldn't match on an OS"
+fi
