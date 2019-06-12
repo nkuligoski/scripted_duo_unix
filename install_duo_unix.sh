62536fbdc304a4fa63c48ed2448beabaa3bf98fe
nick <nick@bigbox-store.com>
2019-06-12 10:11:12 -0400

    trimming the fat

--- install_duo_unix.sh
+++ install_duo_unix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# Purpose: Help administrators install Duo on Ubuntu 14.04, 16.04, or 18.04
+# Purpose: Assist administrators install Duo on CentOS and Ubuntu
 
 # Capture Operating System
 if lsb_release > /dev/null 2>&1 ; then
@@ -10,22 +10,3 @@ else
     # Centos/Redhat - redhat-lsb-core
     # Ubuntu - sb-core
 fi
-
-# Install pam_duo prerequisites
-# Download and extract the latest version of duo_unix
-# Build and install duo_unix with PAM support
-# Update /etc/duo/pam_duo.conf with IKEY, SKEY, and HOST
-# PubKey + Duo or Password + Duo?
-# Configure /etc/pam.d/
-
-
-if [ "$OS" = "Ubuntu 14.04 LTS" ]; then
-	echo "Configuring Duo for $OS"
-elif [ "$OS" = "Ubuntu 16.04 LTS" ]; then
-	echo "Configuring Duo for $OS"
-elif [ "$OS" = "Ubuntu 18.04.2 LTS" ]; then
-	echo "Configuring Duo for $OS"
-else
-	echo "Doing other stuff"
-fi
-
