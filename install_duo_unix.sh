62f3c0137d845ddd90dfb57e39b8e4b3147bfeae
nick <nick@bigbox-store.com>
2019-06-18 15:52:37 -0400

    adding logic to the script. asking for duo key information

--- install_duo_unix.sh
+++ install_duo_unix.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
 # Purpose: Assist administrators install Duo on CentOS and Ubuntu
 
+echo "Please enter your integration key: "
+read integration_key
+
+echo "Please enter your secret key: "
+read secret_key
+
+echo "Please enter your api hostname: "
+read api_hostname
+
 # Capture Operating System
 if [ lsb_release > /dev/null 2>&1 ]; then
     read OS <<< $(lsb_release -d | awk '{ print $2,$3 }')
-    echo "$OS"
+    echo "Detected: $OS"
 else
     echo "'lsb_release' package required to proceed"
     # Centos/Redhat: sudo yum install redhat-lsb-core
@@ -16,7 +25,10 @@ else
 fi
 
 if [ "$OS" = "Ubuntu 16.04.6" ]; then
-	echo "Call 'ubuntu_duo' script"
+	echo "Call 'ubuntu_duo' script (pass in Duo keys)"
+
+	# Duo script 
+	bash ubuntu_duo.sh $integration_key $secret_key $api_hostname
 elif [ "$OS" = "CentOS" ]; then
 	echo "Call 'centos_duo' script"
 else
--- systems/ubuntu_duo.sh
+++ systems/ubuntu_duo.sh
@@ -2,8 +2,29 @@
 # Purpose: Configure Ubuntu with primary authentication (pubkey or password) + Duo
 
 # Install pam_duo prerequisites
+echo "Installing pam_duo prerequisites"
+sudo apt install libssl-dev libpam-dev gcc
+
 # Download and extract the latest version of duo_unix
+echo "Downloading latest version of duo_unix"
+wget https://dl.duosecurity.com/duo_unix-latest.tar.gz
+
+echo "Extracting duo_unix"
+tar zxf duo_unix-latest.tar.gz
+
+echo "Changing into duo_unix directory"
+cd duo_unix-*
+
 # Build and install duo_unix with PAM support
+echo "Building and installing duo_unix with PAM support"
+./configure --with-pam --prefix=/usr && make && sudo make install
+
 # Update /etc/duo/pam_duo.conf with IKEY, SKEY, and HOST
+# echo "Configuring /etc/duo/pam_duo.conf with your Duo keys"
+# sudo sed -i "s/^ikey = .*/ikey = $INTEGRATION_KEY/" /etc/duo/pam_duo.conf
+# sudo sed -i "s/^skey = .*/skey = $SECRET_KEY/" /etc/duo/pam_duo.conf
+# sudo sed -i "s/^host = .*/host = $API_HOSTNAME/" /etc/duo/pam_duo.conf
+# sudo cat /etc/duo/pam_duo.conf
+
 # PubKey + Duo or Password + Duo?
 # Configure /etc/pam.d/
