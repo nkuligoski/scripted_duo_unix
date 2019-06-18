fe6af2df217aac9a4e9b8f15a54eda854a0c823d
nick <nick@bigbox-store.com>
2019-06-18 16:01:44 -0400

    Adding logic

--- systems/ubuntu_duo.sh
+++ systems/ubuntu_duo.sh
@@ -9,30 +9,37 @@ echo "API Hostname: $3"
 echo "Would you like to continue? [yes/no]"
 read continue
 
-# Install pam_duo prerequisites
-echo "Installing pam_duo prerequisites"
-sudo apt install libssl-dev libpam-dev gcc
+if [ "$continue" = "yes" ]; then
+	# Install pam_duo prerequisites
+	echo "Installing pam_duo prerequisites"
+	sudo apt install libssl-dev libpam-dev gcc
 
-# Download and extract the latest version of duo_unix
-echo "Downloading latest version of duo_unix"
-wget https://dl.duosecurity.com/duo_unix-latest.tar.gz
+	# Download and extract the latest version of duo_unix
+	echo "Downloading latest version of duo_unix"
+	wget https://dl.duosecurity.com/duo_unix-latest.tar.gz
 
-echo "Extracting duo_unix"
-tar zxf duo_unix-latest.tar.gz
+	echo "Extracting duo_unix"
+	tar zxf duo_unix-latest.tar.gz
 
-echo "Changing into duo_unix directory"
-cd duo_unix-*
+	echo "Changing into duo_unix directory"
+	cd duo_unix-*
 
-# Build and install duo_unix with PAM support
-echo "Building and installing duo_unix with PAM support"
-./configure --with-pam --prefix=/usr && make && sudo make install
+	# Build and install duo_unix with PAM support
+	echo "Building and installing duo_unix with PAM support"
+	./configure --with-pam --prefix=/usr && make && sudo make install
 
-# Update /etc/duo/pam_duo.conf with IKEY, SKEY, and HOST
-# echo "Configuring /etc/duo/pam_duo.conf with your Duo keys"
-# sudo sed -i "s/^ikey = .*/ikey = $INTEGRATION_KEY/" /etc/duo/pam_duo.conf
-# sudo sed -i "s/^skey = .*/skey = $SECRET_KEY/" /etc/duo/pam_duo.conf
-# sudo sed -i "s/^host = .*/host = $API_HOSTNAME/" /etc/duo/pam_duo.conf
-# sudo cat /etc/duo/pam_duo.conf
+	# Update /etc/duo/pam_duo.conf with IKEY, SKEY, and HOST
+	# echo "Configuring /etc/duo/pam_duo.conf with your Duo keys"
+	# sudo sed -i "s/^ikey = .*/ikey = $INTEGRATION_KEY/" /etc/duo/pam_duo.conf
+	# sudo sed -i "s/^skey = .*/skey = $SECRET_KEY/" /etc/duo/pam_duo.conf
+	# sudo sed -i "s/^host = .*/host = $API_HOSTNAME/" /etc/duo/pam_duo.conf
+	# sudo cat /etc/duo/pam_duo.conf
 
-# PubKey + Duo or Password + Duo?
-# Configure /etc/pam.d/
+	# PubKey + Duo or Password + Duo?
+	# Configure /etc/pam.d/
+elif [ "$continue" = "no" ]; then
+	echo "Exiting"
+	exit 0
+else
+	echo "Please enter 'yes' or 'no'."
+fi
